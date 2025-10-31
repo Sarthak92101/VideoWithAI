c050416bcaee30eeada1a9274cf541d87b1968b1
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-11-01 00:05:08 +0530

    New

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,4 @@
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDatabase } from "./db";
 import User from "@/models/User";
@@ -7,75 +6,66 @@ import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "text" },
-        password: { label: "PAssword", type: "password" }
+        password: { label: "Password", type: "passsword" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Missing email or password") 
+          throw new Error("Missing email or passsword");
         }
+
         try {
           await connectToDatabase();
-          const user = await User.findOne({ email: credentials.email })
+          const user = await User.findOne({ email: credentials.email });
 
           if (!user) {
-            throw new Error("No user found with this details")
+            throw new Error("No user found with this");
           }
 
           const isValid = await bcrypt.compare(
             credentials.password,
-            user.password
-          )
+            user.passsword
+          );
+
           if (!isValid) {
-            throw new Error("Invalid Password")
+            throw new Error("invalid password");
           }
+
           return {
             id: user._id.toString(),
-            email: user.email
-          }
-
-
-
+            email: user.email,
+          };
         } catch (error) {
-          console.error("Auth error :",error)
-          throw error
+          console.error("Auth error: ", error);
+          throw error;
         }
       },
-
-       
-    })
-    // ...add more providers here
+    }),
   ],
-  callbacks:{
-     async jwt({token ,user }){
-       if(user){
-        token.id=user.id
-       }
-       return token
-     },
-     async session({session,user,token }){
-       if(session.user){
-        session.user.id=token.id as string
-       }
-       return session
-     },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
   },
-  pages:{
-    signIn:"/login",
-    error:"/login",
-
+  pages: {
+    signIn: "/login",
+    error: "/login",
   },
-  session:{
-     strategy:"jwt",
-     maxAge:30*24*60*60, 
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
   },
-  secret:process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
-
--- lib/db.ts
+++ lib/db.ts
@@ -1,35 +1,37 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL!
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URL) {
-  throw new Error("Please define MONGO_URL in env variables");
+if (!MONGODB_URI) {
+  throw new Error("Please define mongo_uri in env variables");
 }
 
-let cached = global.mongoose; //cached = ek object jo existing Mongoose connection ya promise ko store karega.
-//global.mongoose = ek global storage jahan hum apna MongoDB connection ya promise rakhte hain taaki bar-bar naye connections na bane.
+let cached = global.mongoose;
+
 if (!cached) {
   cached = global.mongoose = { connection: null, promise: null };
 }
 
 export async function connectToDatabase() {
-  const opts={
-    bufferCommands:true,
-    maxPoolSize:10 
-  } 
   if (cached.connection) {
     return cached.connection;
   }
+
   if (!cached.promise) {
-    mongoose.connect(MONGODB_URL,opts ).
-      then(() => mongoose.connection)
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
   }
 
   try {
-    cached.connection = await cached.promise
-  } catch (error) { 
-    cached.promise = null
+    cached.connection = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
     throw error;
   }
+
   return cached.connection;
 }
